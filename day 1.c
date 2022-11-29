b71020cc409feb6be794098a2aab90e531a1f956
dharmik8488 <118181437+dharmik8488@users.noreply.github.com>
2022-11-29 19:14:19 +0530

    Add files via upload

---
+++ day 1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+  main(){
+  	  int p;
+  	  
+  	  printf("enter size of array:-");
+  	  scanf("%i",&p);
+  	  int a[p];
+  	  
+  	  int i;
+  	  
+  	  for(i=0; i<p; i++){
+  	  	
+  	  	scanf("%i",&a[i]);
+  	  	
+		}
+  	 for(i=0; i<p; i++){
+  	 	
+  	 	printf("%i\n",a[i]);
+	   }
+  	
+  	
+  	
+  	
+  	
+  	
+  	
+  }
---
+++ day 2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+  main(){
+  	 
+	  
+  	  
+  	  int n[]={10,20,30,40,50,60};
+  	  
+  	  int a= sizeof(n);
+  	  int c= sizeof(int);
+  	  
+  	  int l=a/c;
+  	  
+  	  printf("length= %d\n",l);
+	   
+	   
+}
+	   
+	   
+	   
+	   
+	   
+  	 
+  	
+  	
